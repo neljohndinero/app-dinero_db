ba8c633a2a6d3e62e9cf48aa1cfc5500393e72d0
Nel <dineroneljohn@gmail.com>
2024-03-05 12:07:26 +0800

    intl commit

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layout.main')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Register') }}</div>
+                            <div class="form-group">
+                                <label for="name">{{ __('Name') }}</label>
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="Gender">{{ __('Gender') }}</label>
+                                <input id="Gender" type="email" class="form-control" name="Gender" value="{{ old('') }}" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="Age">{{ __('Age') }}</label>
+                                <input id="Age" type="Age" class="Age" name="Age" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password">{{ __('Password') }}</label>
+                                <input id="password" type="password" class="form-control" name="password" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password-confirm">{{ __('Confirm Password') }}</label>
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
