c225047cc513541bc8b6948421e0fb04e11bd956
Nel <dineroneljohn@gmail.com>
2024-03-12 11:56:12 +0800

    intl commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\HTTP\Controllers\GenderController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('\gender',[GenderController::class,'index']);
 
-Route::get('/users',function(){
-    return view('user.index');
-});
